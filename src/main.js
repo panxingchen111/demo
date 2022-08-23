7fa7563928899c0703414ca658b509e6b2a3a441
panxingchen <panxingchen@orientsec.com.cn>
2022-08-23 09:41:24 +0800

    切换路由时添加进度条

--- src/main.js
+++ src/main.js
@@ -14,7 +14,9 @@ import Viewer from 'v-viewer'
 import 'viewerjs/dist/viewer.css'
 import dayjs from "dayjs"
 import checkRights from "./utils/checkRights";
-process.env.VUE_APP_MOCK === 'true' && require('./mock/index'); 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css' // nprogress样式文件
+process.env.VUE_APP_MOCK === 'true' && require('./mock/index');
 // 挂载到Vue实力上，全局可通过this.$store进行调用
 Vue.use(ElementUI);
 Vue.prototype.$FormValidate = FormValidate; //  2、注册成全局验证
@@ -42,6 +44,28 @@ Viewer.setDefaults({
     "url": "data-source"
   }
 });
+NProgress.configure({
+  easing: 'ease', // 动画方式
+  speed: 500, // 递增进度条的速度
+  showSpinner: false, // 是否显示加载ico
+  trickleSpeed: 200, // 自动递增间隔
+  minimum: 0.3 // 初始化时的最小百分比
+})
+
+//当路由进入前
+router.beforeEach((to, from, next) => {
+  // 每次切换页面时，调用进度条
+  NProgress.start();
+  next()
+  // 若加载时间长且不定，担心进度条走完都没有加载完，可以调用　　NProgress.inc（）；//这会以随机数量递增，且永远达不到100%，也可以设指定增量    next();
+});
+//当路由进入后：关闭进度条
+router.afterEach(() => {
+  // 在即将进入新的页面组件前，关闭掉进度条
+  NProgress.done()
+})
+
+
 new Vue({
   el: '#app',
   router,
